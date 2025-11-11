f02af13ecc8c8ffdd19c54edf9311e01feab17f3
Gavin Grant <gavingrant@gmail.com>
2025-11-10 22:38:01 -0800

    feat(home): background video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import SearchInput from "@/components/search-input"
+import HomeVideo from "@/components/home-video"
 
 export default async function Home() {
   return (
     <>
-      <div className="antialiased bg-[url(/images/home.webp)] bg-cover bg-center absolute top-0 left-0 w-full h-full -z-10 bg-fixed" />
-      <div className="bg-cover bg-center absolute top-0 left-0 w-full h-full bg-linear-to-t from-transparent to-white -z-10" />
+      <div className="bg-cover bg-center fixed top-0 left-0 w-full h-full bg-linear-to-t from-transparent to-white -z-10" />
+      <HomeVideo />
 
       <div className="top-0 left-0 w-full h-full flex flex-col items-center justify-center min-h-[calc(100vh-80px)]">
         <div className="w-full max-w-md fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-4">
---
+++ src/components/home-video.tsx
@@ -0,0 +1,17 @@
+export default function HomeVideo() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full -z-20">
+      <video
+        playsInline
+        autoPlay
+        muted
+        loop
+        preload="auto"
+        id="home-video"
+        className="w-full h-full object-cover"
+      >
+        <source src="/videos/home.mp4" type="video/mp4" />
+      </video>
+    </div>
+  )
+}
