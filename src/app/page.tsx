d16971b2fe99be2279b4b01005f8f0f7bb5dc3a8
Gavin Grant <gavingrant@gmail.com>
2025-11-16 16:50:33 -0800

    refactor(home): text layout adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ export default async function Home() {
 
       <div className="top-0 left-0 w-full flex flex-col items-center justify-center h-full">
         <div className="w-full fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-4 text-center">
-          <h1 className="text-3xl font-bold">
+          <h1 className="text-2xl sm:text-3xl font-bold leading-7 sm:leading-8">
             Buying less plastic means less plastic waste
           </h1>
-          <p className="text-sm mb-8 mt-2">
+          <p className="text-sm mb-8 mt-4 sm:mt-3">
             Find products that use less or no plastic. Search by keyword or
             select a category to get started.
           </p>
@@ -26,7 +26,7 @@ export default async function Home() {
               placeholder="Search by keyword"
               padding="py-5"
             />
-            <div className="flex flex-wrap items-center justify-center sm:justify-between gap-2 mt-4">
+            <div className="flex flex-wrap items-center justify-center sm:justify-between gap-3 mt-4">
               {categoryOptions.map((category) => {
                 if (category.value !== "all") {
                   return (
