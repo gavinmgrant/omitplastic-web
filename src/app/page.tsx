aa7fff078d9916471e9c85adee2dbd5faedefcc3
Gavin Grant <gavingrant@gmail.com>
2025-11-17 18:01:09 -0800

    refactor(home): update background video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,15 @@ import Link from "next/link"
 export default async function Home() {
   return (
     <>
-      <div className="bg-cover bg-center fixed top-0 left-0 w-full h-full bg-linear-to-t from-transparent to-white -z-10" />
+      <div className="bg-cover bg-center fixed top-0 left-0 w-full h-full bg-linear-to-t from-white/5 to-white to-95% backdrop-blur-xs -z-10" />
       <HomeVideo />
 
-      <div className="top-0 left-0 w-full flex flex-col items-center justify-center h-full">
+      <div className="top-0 left-0 w-full flex flex-col items-center justify-center h-full z-10">
         <div className="w-full fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-4 text-center">
-          <h1 className="text-2xl sm:text-3xl font-bold leading-7 sm:leading-8">
+          <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold leading-7 sm:leading-8">
             Buying less plastic means less plastic waste
           </h1>
-          <p className="text-sm mb-8 mt-4 sm:mt-3">
+          <p className="mb-8 mt-4 sm:mt-3">
             Find products that use less or no plastic. Search by keyword or
             select a category to get started.
           </p>
