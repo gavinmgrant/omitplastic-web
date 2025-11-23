dcefc3fa5291e6bb2b11c3b3af4eb124be731241
Gavin Grant <gavingrant@gmail.com>
2025-11-23 12:17:37 -0800

    refactor(config): change to shopping-bags

--- src/config/categories.ts
+++ src/config/categories.ts
@@ -1,14 +1,14 @@
 export const categories = {
-  bags: "5177ceca-5460-45d1-9333-a5bb5a8b511f",
-  bottles: "de866af6-5874-4db3-aeb2-40f6f95b7d38",
+  "shopping-bags": "5177ceca-5460-45d1-9333-a5bb5a8b511f",
+  "bottles": "de866af6-5874-4db3-aeb2-40f6f95b7d38",
   "food-kitchen": "359811fb-b4f6-40df-9c78-2b9de86396d4",
   "household-supplies": "f26c3303-e610-4c6a-9e9a-aa86ed9aaff1",
-  "personal-care": "f302df97-39b2-4174-98e8-d2a513604b6a",
+  "personal-care-and-cleaning": "f302df97-39b2-4174-98e8-d2a513604b6a",
 }
 
 export const categoryOptions = [
   { label: "All", value: "all" },
-  { label: "Bags", value: "bags" },
+  { label: "Shopping Bags", value: "shopping-bags" },
   { label: "Bottles", value: "bottles" },
   { label: "Food & Kitchen", value: "food-kitchen" },
   { label: "Household Supplies", value: "household-supplies" },
