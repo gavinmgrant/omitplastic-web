cb42bb001395b892a1b348e0cad4293c553063ed
Gavin Grant <gavingrant@gmail.com>
2025-11-25 11:44:16 -0800

    refactor(sitemap): add guides

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,6 @@
 import { MetadataRoute } from "next"
 import { getProducts } from "@/lib/products"
+import { reader } from "./keystatic/reader"
 
 const baseUrl = "https://www.omitplastic.com"
 
@@ -14,6 +15,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: `${baseUrl}/products`,
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/guides`,
+      lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
@@ -23,9 +30,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const dynamicEntries: MetadataRoute.Sitemap = products.map((product) => ({
     url: `${baseUrl}/products/${product.slug}`,
     lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.8,
+  }))
+
+  // Get the Keystatic filenames in content/guides and add them to the dynamic entries
+  const guides = await reader.collections.guides.all()
+  const guideEntries: MetadataRoute.Sitemap = guides.map((guide) => ({
+    url: `${baseUrl}/guides/${guide.slug}`,
+    lastModified: new Date(),
     changeFrequency: "monthly",
     priority: 0.8,
   }))
 
-  return [...staticEntries, ...dynamicEntries]
+  return [...staticEntries, ...dynamicEntries, ...guideEntries]
 }
